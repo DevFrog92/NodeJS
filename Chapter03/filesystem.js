373d9950fd04f4a35bd2f55ddf0b1c0bc2d590f0
DevFrog92 <yongsu1481@gmail.com>
2022-08-24 23:32:28 +0900

    ✨ Implement timer and filesystem example

---
+++ Chapter03/filesystem.js
@@ -0,0 +1,4 @@
+// 기본적으로 path module과 함께 사용한다.
+
+console.log(__filename);
+console.log(__dirname);
---
+++ Chapter03/timer.js
@@ -0,0 +1,8 @@
+// setTimeout
+// setInterval
+// setImmediate
+
+// 아래 두 타이머 함수는 동작 방식이 다름
+
+// setTimeout(callback, 0) -> 사용하지 않는 것을 권장
+// setImmediate(callback) -> 대신 사용
